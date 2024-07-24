8c785a462a66e83769d89a6df56d0d72ce52925f
Gabriel CC <ccgabriel.888@gmail.com>
2024-07-23 20:43:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_slot_machine_spin(rows, cols, symbols):
 
     return columns
 
-def print_slot_machine_spin(columns):
+def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
