fbf59602bcd5cafc80a95440b8fbc097ddc40e58
Gabriel CC <ccgabriel.888@gmail.com>
2024-07-23 20:47:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
-symbols_count = {
+symbol_count = {
     "A": 2,
     "B": 4,
     "C": 6,
     "D": 10,
 }
 
-symbols_value = {
+symbol_value = {
     "A": 5,
     "B": 4,
     "C": 3,
