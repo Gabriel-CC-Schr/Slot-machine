a202b15bd26ddf81f7ceb38c81e29533cf682f3a
Gabriel CC <ccgabriel.888@gmail.com>
2024-07-23 20:44:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -67,17 +67,17 @@ def print_slot_machine(columns):
 
 
 
-def desposit():
-    while True: 
-        amount = input("How much would you like to deposit? $")
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print(" Amount must be greater than 0")
+                print("Amount must be greater than 0.")
         else:
-            print("Please enter a valid amount")
+            print("Please enter a number.")
 
     return amount
 
