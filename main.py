3ec162b55df7ed6d9ef3510a60f80a64c935f817
Gabriel CC <ccgabriel.888@gmail.com>
2024-07-23 20:45:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-MAX_Lines = 3
+MAX_LINES = 3
 MAX_Bet = 100
 MIN_Bet = 1
 
@@ -82,16 +82,17 @@ def deposit():
     return amount
 
 def get_number_of_lines():
-    while True: 
-        lines = input("Enter the number of lines to bet on (1- " + str(MAX_Lines) + ")? ")
+    while True:
+        lines = input(
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
         if lines.isdigit():
             lines = int(lines)
-            if 1 <= lines <= MAX_Lines:
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print("Enter a number between 1 and " + str(MAX_Lines))
+                print("Enter a valid number of lines.")
         else:
-            print("Please enter a number")
+            print("Please enter a number.")
 
     return lines
 
